5148b970ce46d2ee6120bfd66ae302349981f769
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-25 18:03:02 -0600

    changed localhost parameter in connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/recipes', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/recipes', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
